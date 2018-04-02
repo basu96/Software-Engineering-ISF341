cc6bd8d5e7ff3995c053a0df8e8b07cba5fd11cb
Basu Dubey <basu.96@gmail.com>
2018-04-02 16:06:00 +0530

    Add User model admin

--- patient-monitoring/project/pm/admin.py
+++ patient-monitoring/project/pm/admin.py
@@ -2,4 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+class UserAdmin(UserAdmin):
+
+    fieldsets = [
+        ('User Info', {
+            'fields': ['username', 'password', 'user_type', ]
+            }),
+        ('Personal info', {
+            'fields': [
+                'first_name',
+                'last_name',
+                'phone',
+                'email',
+                'date_of_birth',
+                'gender',
+                'blood_group',
+            ]
+            }),
+        ('Permissions', {
+            'fields': ['is_active', 'is_staff', 'is_superuser',]
+            }),
+    ]
+
 admin.site.register(User, UserAdmin)
