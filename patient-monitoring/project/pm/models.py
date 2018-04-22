6a73da138b3d6a2291f8a976ca2e002af7d2d3d6
Basu Dubey <basu.96@gmail.com>
2018-04-22 16:55:03 +0530

    Fix blood group length in User model

--- patient-monitoring/project/pm/models.py
+++ patient-monitoring/project/pm/models.py
@@ -39,7 +39,7 @@ class User(AbstractUser):
         ('Other','Other'),
     )
     blood_group = models.CharField(
-        max_length = 3,
+        max_length = 5,
         choices = BLOOD_GROUP_CHOICES,
         null = True,
         default = 'Other',
